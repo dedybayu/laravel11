c8dfbbe2b3d5b9602ad7b44e15492122ee1ba3d7
Dedy Bayu Setiawan <dedybayusetiawan5@gmail.com>
2025-02-16 17:03:52 +0700

    add: Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,42 +1,14 @@
 <?php
+
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
+class Post extends Model
+{
+    //
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug','body'];
 
-class Post{
-    public static function all(){
-        return [[
-            'id' => '1',
-            'slug'=> 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Bayu',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, corrupti vero dolores
-                        pariatur aut sunt veniam suscipit atque saepe quibusdam neque obcaecati veritatis enim alias. Eveniet nostrum
-                        facere cumque itaque.'
-        ],
-        [
-            'id' => '2',
-            'slug'=> 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Aaaaa',
-            'body' => 'psum dolor sit amet consectetur adipisicing elit. Corporis, corrupti vero dolores
-                        pariatur aut sunt veniam suscipit atque saepe quibusdam neque obcaecati veritatis enim alias. Eveniet nostrum
-                        facere cumque itaque.'
-        ]];
-    }
-
-    public static function find($slug): array  { 
-        
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-        if (!$post) {
-            abort(404);
-        } else {
-            return $post;
-        }
-
-        // return Arr::first(static::all(), function($post) use ($slug){
-        //     return $post['slug'] == $slug;
-        // });
-    }
 }
