7b2d92eb42f85d88f72b63726fa1ed967aec8a30
Dedy Bayu Setiawan <dedybayusetiawan5@gmail.com>
2025-02-16 18:09:45 +0700

    fix: bug logo dan tanggal

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,7 +3,7 @@
       <div class="flex h-16 items-center justify-between">
         <div class="flex items-center">
           <div class="shrink-0">
-            <img class="size-8" src="img/logo1.png" alt="Your Company">
+            <img class="size-8" src="/../img/logo1.png" alt="Your Company">
           </div>
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
