3357a52ef897bd27bb8fb22611f6f305a3ddf38d
Dedy Bayu Setiawan <dedybayusetiawan5@gmail.com>
2025-02-17 15:51:18 +0700

    Upload semua source Laravel

---
+++ storage/framework/views/2dee8847a3ef83b97245e1d402a94387.php
@@ -0,0 +1,83 @@
+<?php if (isset($component)) { $__componentOriginal1f9e5f64f242295036c059d9dc1c375c = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal1f9e5f64f242295036c059d9dc1c375c = $attributes; } ?>
+<?php $component = App\View\Components\Layout::resolve([] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('layout'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\App\View\Components\Layout::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+   <?php $__env->slot('title', null, []); ?> <?php echo e($title); ?> <?php $__env->endSlot(); ?>
+
+  
+
+  <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-8 lg:px-0">
+    <div class="grid gap-8 lg:grid-cols-3 md:grid-cols-2">
+
+      <?php $__currentLoopData = $posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+      <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <div class="flex justify-between items-center mb-5 text-gray-500">
+
+        <a href="/categories/<?php echo e($post->category->slug); ?>">
+        <span
+          class="bg-<?php echo e($post->category->color); ?>-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+          <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd"
+            d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
+            clip-rule="evenodd"></path>
+          <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path>
+          </svg>
+          <?php echo e($post->category->name); ?>
+
+        </span>
+        </a>
+
+
+        <span class="text-sm"><?php echo e($post->created_at->diffForHumans()); ?></span>
+      </div>
+      <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a
+        href="/posts/<?php echo e($post['slug']); ?>"><?php echo e($post['title']); ?></a></h2>
+      <p class="mb-5 font-light text-gray-500 dark:text-gray-400"><?php echo e(Str::limit($post['body'], 100)); ?></p>
+      <div class="flex justify-between items-center">
+        <div class="flex items-center space-x-4">
+        <a href="/authors/<?php echo e($post->author->username); ?>">
+          <img class="w-7 h-7 rounded-full" src="/../img/user.png" alt="Jese Leos avatar" />
+        </a>
+        <a href="/authors/<?php echo e($post->author->username); ?>">
+          <span class="font-medium dark:text-white">
+          <?php echo e(Str::words($post->author->name, 2, '')); ?>
+
+
+          </span>
+        </a>
+
+
+        </div>
+        <a href="/posts/<?php echo e($post['slug']); ?>"
+        class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+        Read more
+        <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd"
+          d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+          clip-rule="evenodd"></path>
+        </svg>
+        </a>
+      </div>
+      </article>
+    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+    </div>
+  </div>
+
+
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal1f9e5f64f242295036c059d9dc1c375c)): ?>
+<?php $attributes = $__attributesOriginal1f9e5f64f242295036c059d9dc1c375c; ?>
+<?php unset($__attributesOriginal1f9e5f64f242295036c059d9dc1c375c); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal1f9e5f64f242295036c059d9dc1c375c)): ?>
+<?php $component = $__componentOriginal1f9e5f64f242295036c059d9dc1c375c; ?>
+<?php unset($__componentOriginal1f9e5f64f242295036c059d9dc1c375c); ?>
+<?php endif; ?><?php /**PATH C:\public_html\laravel11\resources\views/posts.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/b00c43230774517bd7bb2dd2f38f8216.php
@@ -0,0 +1,73 @@
+<?php if (isset($component)) { $__componentOriginal1f9e5f64f242295036c059d9dc1c375c = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal1f9e5f64f242295036c059d9dc1c375c = $attributes; } ?>
+<?php $component = App\View\Components\Layout::resolve([] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('layout'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\App\View\Components\Layout::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+   <?php $__env->slot('title', null, []); ?> <?php echo e($title); ?> <?php $__env->endSlot(); ?>
+
+  
+
+
+  <!-- 
+Install the "flowbite-typography" NPM package to apply styles and format the article content: 
+
+URL: https://flowbite.com/docs/components/typography/ 
+-->
+
+<main class="pt-4 pb-8 lg:pt-8 lg:pb-8 bg-white dark:bg-gray-900 antialiased">
+  <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
+      <article class="mx-auto w-full max-w-5xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+          <header class="mb-4 lg:mb-6 not-format">
+              <a href="/posts" class="font-medium text-sm text-blue-600 hover:underline">&laquo; Back</a>
+              <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white mt-6"><?php echo e($post['title']); ?></h1>
+          </header>
+
+          <p><?php echo e($post['body']); ?></p>
+          
+          <div class="mb-4 lg:mb-6 not-format mt-10">
+            <h3 class="mb-4 text-2xl font-bold leading-tight text-gray-800 lg:mb-6 lg:text-2xl dark:text-white">Author:</h3>
+            <address class="flex items-center mb-6 not-italic">
+              <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                  <img class="mr-4 w-16 h-16 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="Jese Leos">
+                  <div>
+                      <a href="/authors/<?php echo e($post->author->username); ?>" rel="author" class="text-xl font-bold text-gray-900 dark:text-white"><?php echo e($post->author->name); ?></a>
+                      <p class="text-base text-gray-500 dark:text-gray-400"><time pubdate datetime="2022-02-08" title="February 8th, 2022"><?php echo e($post->created_at->format('j F Y')); ?></time></p>
+                      <a href="/categories/<?php echo e($post->category->slug); ?>">
+                        <span
+                          class="bg-<?php echo e($post->category->color); ?>-100 text-primary-800 text-x font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                          <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                          <path fill-rule="evenodd"
+                            d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
+                            clip-rule="evenodd"></path>
+                          <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path>
+                          </svg>
+                          <?php echo e($post->category->name); ?>
+
+                        </span>
+                        </a>
+                  </div>
+              </div>
+          </address>
+          </div>
+      </article>
+  </div>
+</main>
+
+
+
+
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal1f9e5f64f242295036c059d9dc1c375c)): ?>
+<?php $attributes = $__attributesOriginal1f9e5f64f242295036c059d9dc1c375c; ?>
+<?php unset($__attributesOriginal1f9e5f64f242295036c059d9dc1c375c); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal1f9e5f64f242295036c059d9dc1c375c)): ?>
+<?php $component = $__componentOriginal1f9e5f64f242295036c059d9dc1c375c; ?>
+<?php unset($__componentOriginal1f9e5f64f242295036c059d9dc1c375c); ?>
+<?php endif; ?><?php /**PATH C:\public_html\laravel11\resources\views/post.blade.php ENDPATH**/ ?>
